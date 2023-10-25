f9c361ce34e2f7280b3d47d67447cba014f1065f
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-10-25 16:23:17 -0400

    removed links redirecting to flowbite

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,8 +11,8 @@ export default function Navbar(){
             
                 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                        <a href="https://flowbite.com/" className="flex items-center">
-                            {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"> */}
+                        <a href="/" className="flex items-center">
+                            {/* <img src="/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"> */}
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Workout Log</span>
                         </a>
                         <div className="flex md:order-2">
@@ -49,8 +49,8 @@ export default function Navbar(){
             <>
                 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                        <a href="https://flowbite.com/" className="flex items-center">
-                            {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"> */}
+                        <a href="/" className="flex items-center">
+                            {/* <img src="/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo"> */}
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Workout Log</span>
                         </a>
                         <div className="flex md:order-2">
