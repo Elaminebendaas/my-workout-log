a419043e5d812f11a47f33e20989618da0b797bf
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-11-08 14:19:36 -0500

    made navbar dynamic

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -28,20 +28,36 @@ export default function Navbar(){
                                 </svg>
                             </Button>
                         </div>
-                        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <div className="items-center justify-between  hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                            <ul className="flex flex-col p-4 md:p-0 items-center mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
-                                    <Link href="/home" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                                    {
+                                        pathName === '/home' ?
+                                        <Link href="/home" className="block py-2 pl-3 pr-4 rounded text-blue-700  font-semibold" >Home</Link>
+                                        :
+                                        <Link href="/home" className="block py-2 pl-3 pr-4 text-gray-900 bg-blue-700 rounded md:bg-transparent  md:p-0 md:dark:text-blue-500" >Home</Link>
+                                    }
+                                    
                                 </li>
                                 <li>
+                                    {pathName === '/exercises' ?
+                                    <Link href="/exercises" className="block py-2 pl-3 pr-4 rounded text-blue-700  font-semibold">Excercises</Link>
+                                    :
                                     <Link href="/exercises" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Excercises</Link>
+                                    }
+                                    
                                 </li>
                                 <li>
+                                    {pathName === '/workouts' ?
+                                    <Link href="/workouts" className="block py-2 pl-3 pr-4 rounded text-blue-700  font-semibold ">Workouts</Link>
+                                    :
                                     <Link href="/workouts" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Workouts</Link>
+                                }
+                                    
                                 </li>
                                 <li>
                                 {pathName === '/cardio' ? 
-                                <Link href="/cardio" className="block  py-2 pl-3 pr-4 text-gray-900 rounded bg-gray-300 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Cardio</Link>
+                                <Link href="/cardio" className="block py-2 pl-3 pr-4 rounded text-blue-700  font-semibold ">Cardio</Link>
                                 : 
                                 <Link href="/cardio" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Cardio</Link>}
                                 </li>
