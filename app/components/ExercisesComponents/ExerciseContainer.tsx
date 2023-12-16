255bca81dc31aca5db3bb52b7413b3f4ae0869bc
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-12-16 17:29:02 -0500

    changes

--- app/components/ExercisesComponents/ExerciseContainer.tsx
+++ app/components/ExercisesComponents/ExerciseContainer.tsx
@@ -1,11 +1,13 @@
 
 import ExcerciseItem from "./ExerciseItem"
 import { fetchExercises } from "@/app/exercises/actions"
+import { Excercise } from "@/lib/declaration"
+import { Exercises } from "@prisma/client"
 import { useEffect, useState, useRef } from "react"
 
 
 
-export default  function ExcerciseContainer({email}: {email: string}){
+export default function ExcerciseContainer({email}: {email: string}){
 
     const [exercises, setExercises] = useState<any>()
     const myExercises  = useRef(exercises)
@@ -23,8 +25,10 @@ export default  function ExcerciseContainer({email}: {email: string}){
     return(
     <>
         <div className="flex flex-col bg-gray-200 bg-opacity-50 rounded-md mt-4 p-2">
-            <ExcerciseItem />
-            <ExcerciseItem />
+            {myExercises.current?.length === 0 ? <p></p> : myExercises.current?.map((key: number, item: Excercise) => {
+                {console.log(item)}
+                <p>{item.name}</p> 
+            })}
         </div>
     </>)
 }
--- lib/declaration.ts
+++ lib/declaration.ts
@@ -1,5 +1,12 @@
 export type Excercise = {
     id: string
-    exercise_name: string
-    
+    name: string
+    ownerEmail: string;
+} & {
+    history: {
+        date: Date;
+        weightLBS: number;
+        reps: number;
+        sets: number;
+    }   
 }
