ffddb65505fbc14b1e0205a20c721d0e3e2e453e
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-12-17 13:18:42 -0500

    you can now create exercises and see them

--- app/components/ExercisesComponents/ExerciseContainer.tsx
+++ app/components/ExercisesComponents/ExerciseContainer.tsx
@@ -1,7 +1,6 @@
 
 import ExcerciseItem from "./ExerciseItem"
 import { fetchExercises } from "@/app/exercises/actions"
-import { Excercise } from "@/lib/declaration"
 import { Exercises } from "@prisma/client"
 import { useEffect, useState, useRef } from "react"
 
@@ -10,7 +9,7 @@ import { useEffect, useState, useRef } from "react"
 export default function ExcerciseContainer({email}: {email: string}){
 
     const [exercises, setExercises] = useState<any>()
-    const myExercises  = useRef(exercises)
+    const myExercises  = useRef<Exercises[] | false>(exercises)
     
     useEffect( () => {
         const fetch = async () =>{
@@ -21,13 +20,14 @@ export default function ExcerciseContainer({email}: {email: string}){
         fetch()
     },[email, myExercises])
 
-
+    
     return(
     <>
         <div className="flex flex-col bg-gray-200 bg-opacity-50 rounded-md mt-4 p-2">
-            {myExercises.current?.length === 0 ? <p></p> : myExercises.current?.map((key: number, item: Excercise) => {
-                {console.log(item)}
-                <p>{item.name}</p> 
+            {myExercises.current === false ||  myExercises.current?.length === 0   ? <p>You currently have no exercises. Go ahead and create one!</p> : myExercises.current?.map((item: Exercises, key: number) => {
+                return(                    
+                <ExcerciseItem key={key} workout={item}/>
+                ) 
             })}
         </div>
     </>)
--- app/components/ExercisesComponents/ExerciseItem.tsx
+++ app/components/ExercisesComponents/ExerciseItem.tsx
@@ -1,8 +1,13 @@
 import { useSession } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { Exercises } from "@prisma/client"
 
-export default function ExcerciseItem(){
+interface IWorkoutProps {
+  workout: Exercises
+}
+
+export default function ExcerciseItem({ workout }: IWorkoutProps){
 
     const { data: session, status } = useSession()
 
@@ -16,14 +21,14 @@ export default function ExcerciseItem(){
     <>
       <div className="flex justify-between align-center rounded m-2 bg-white p-4">
 
-            <p className="font-semibold">Deadlift</p>
+            <p className="font-semibold">{workout.name}</p>
 
             <div className="flex justify-center align-center gap-2">
               <Button asChild variant={'secondary'}> 
-                  <Link href={'/'}>View</Link>
+                  <Link href={`/${workout.id}`}>View</Link>
               </Button>
               <Button asChild variant={'destructive'}> 
-                <Link href={'/'}>Delete</Link>
+                <Link href={`/${workout.id}/`}>Delete</Link>{/* Replace the Link with a button since you want an onclick function with this to delete the workout */}
               </Button> 
             </div>
 
