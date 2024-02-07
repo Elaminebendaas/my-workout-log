c4e82537b7204e76fdd14f7f51816cc17fbc0a4f
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2024-02-06 21:08:30 -0500

    made things prettier

--- lib/declaration.ts
+++ lib/declaration.ts
@@ -1,12 +1,12 @@
 export type Excercise = {
-    id: string
-    name: string
-    ownerEmail: string;
+  id: string;
+  name: string;
+  ownerEmail: string;
 } & {
-    history: {
-        date: Date;
-        weightLBS: number;
-        reps: number;
-        sets: number;
-    }   
-}
+  history: {
+    date: Date;
+    weightLBS: number;
+    reps: number;
+    sets: number;
+  };
+};
